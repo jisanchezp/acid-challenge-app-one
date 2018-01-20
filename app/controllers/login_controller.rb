2ea7bc86bf4853bfd7f6baf7450b319adc664728
jisanchezp <juanisanchez@outlook.com>
2018-01-20 15:07:02 -0300

    Add partial logic to login controller validate_request action

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,10 +1,16 @@
 class LoginController < ApplicationController
+  require 'httparty'
+
 
   def validate
     @login = Login.new
   end
 
   def validate_request
+    urlstring_to_post = "http://localhost:3000/#" # \#  TODO: replace with  REST API call url
+    response = HTTParty.post(urlstring_to_post.to_str, :body => params[:login].to_json, :headers => { 'Content-Type' => 'application/json' } )
 
+    parsed_response = JSON.parse(response)
+    render json: response
   end
 end
