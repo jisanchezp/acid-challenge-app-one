3a83d5e8d9aa5ad0b90443eabf2b30ee2956a82f
jisanchezp <juanisanchez@outlook.com>
2018-01-21 15:44:34 -0300

    Modify Login controller and routes. Add validate_request action.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,10 +7,7 @@ class LoginController < ApplicationController
   end
 
   def validate_request
-    urlstring_to_post = "http://localhost:3000/#" # \#  TODO: replace with  REST API call url
-    response = HTTParty.post(urlstring_to_post.to_str, :body => params[:login].to_json, :headers => { 'Content-Type' => 'application/json' } )
-
-    parsed_response = JSON.parse(response)
-    render json: response
-  end
+    urlstring_to_post = "http://localhost:3002/rest/login" # TODO: replace with global variable
+    @response = HTTParty.post(urlstring_to_post.to_str, :body => params[:login].to_json, :headers => { 'Content-Type' => 'application/json' } )
+    end
 end
