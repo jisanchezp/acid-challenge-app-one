8e06c70f239020581584f89e18da63b5fb2a2ad9
jisanchezp <juanisanchez@outlook.com>
2018-01-21 17:25:31 -0300

    Add API_HOST variable to application controller. Modify HTTParty request to have dynamic host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  API_HOST = (Rails.env.development? or Rails.env.test?) ?  "http://localhost:3002" : "https://login-jisanchez.herokuapp.com"
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,13 +1,11 @@
 class LoginController < ApplicationController
   require 'httparty'
 
-
   def validate
     @login = Login.new
   end
 
   def validate_request
-    urlstring_to_post = "http://localhost:3002/rest/login" # TODO: replace with global variable
-    @response = HTTParty.post(urlstring_to_post.to_str, :body => params[:login].to_json, :headers => { 'Content-Type' => 'application/json' } )
+    @response = HTTParty.post("#{API_HOST}/rest/login", :body => params[:login].to_json, :headers => { 'Content-Type' => 'application/json' } )
     end
 end
