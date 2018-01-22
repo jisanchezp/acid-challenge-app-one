073b8722957273f65b174b83c9dc3277b515569a
jisanchezp <juanisanchez@outlook.com>
2018-01-21 21:41:21 -0300

    Fix API-HOST url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  API_HOST = (Rails.env.development? or Rails.env.test?) ?  "http://localhost:3002" : "https://login-jisanchez.herokuapp.com"
+  API_HOST = (Rails.env.development? or Rails.env.test?) ?  "http://localhost:3002" : "https://login-api-jisanchez.herokuapp.com/"
 end
