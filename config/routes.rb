59dd692c6e786bfdca659fda10d9ccaaea568596
jisanchezp <juanisanchez@outlook.com>
2018-01-24 14:17:49 -0300

    Redesign login controller and model with validations. Modify README.md

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  root 'login#validate'
-  get 'validate/', to: 'login#validate'
-  post 'validate/', to: 'login#validate_request'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'logins#new'
+  resources :logins, :only => [:new, :create]
 end
